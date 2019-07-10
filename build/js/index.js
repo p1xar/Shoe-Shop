d61bd7850f22c4ab19e81b1d8a23fd1bf75795c7
p1xar <melnichukr@ya.ru>
2019-07-10 23:48:28 +0300

    Add navigation

---
+++ build/js/index.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("overlay-menu").style.height = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("overlay-menu").style.height = "0";
+  }
